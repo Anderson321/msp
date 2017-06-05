f1548e4ef809eee0f958aafe7a50112b4f0d1b8b
Matthew Valuet <mwvaluet@gmail.com>
2017-06-05 12:04:18 -0700

    Commenting objects in computerVision

--- src/computerVision.cpp
+++ src/computerVision.cpp
@@ -15,16 +15,13 @@ namespace cv {
    */
 	computerVision::computerVision(const char *pipe) {
 		this->pipe = pipe;
-
 		readPipe(pipe);
-		
 	}
 
 	/*
    * Updates the fields by accepting a new pipe name.
    */
 	void computerVision::update(const char *pipe, prevPoint *prev) {
-
 		int tempHor = this->horizontalPixels;
 		int tempVer = this->verticalPixels;
 
@@ -33,46 +30,80 @@ namespace cv {
 		if (this->horizontalPixels != -1 && this->verticalPixels != -1) {
 			prev->update(tempHor, tempVer);
 		}
-	
 	}
 
-	// returns if the line is within vertical and horizontal bounds
+	/* 
+   * Returns true if the line is within vertical and
+   * horizontal bounds, false otherwise.
+   */
 	bool computerVision::hasShoes() {
-		return ((verticalPixels <= V_RANGE && verticalPixels >= 0) &&
-				(horizontalPixels <= H_RANGE && horizontalPixels >= 0));
+		return ((verticalPixels <= V_RANGE && verticalPixels >= 0)
+            && (horizontalPixels <= H_RANGE && horizontalPixels >= 0));
 	}
 
-	// returns if the shoelace is centered. Revisit these values
+	/*
+   * Returns true if the shoelace is centered within the
+   * frame, false otherwise.
+   */
 	bool computerVision::isCentered() {
-		return (horizontalPixels <= H_CENTERRIGHT && horizontalPixels >= H_CENTERLEFT && 
-				verticalPixels <= V_CENTERBOTTOM && verticalPixels >= V_CENTERTOP);
+		return (horizontalPixels <= H_CENTERRIGHT
+            && horizontalPixels >= H_CENTERLEFT
+            && verticalPixels <= V_CENTERBOTTOM
+            && verticalPixels >= V_CENTERTOP);
 	}
 
+  /*
+   * Returns true if the shoelaces are centered horizontally
+   * within the frame, false otherwise.
+   */
 	bool computerVision::isCloseCentered() {
 		return (horizontalPixels <= H_CENTERRIGHT && horizontalPixels >= H_CENTERLEFT);
 	}
 
+  /*
+   * Returns true if the shoelaces are too far to the left,
+   * false otherwise.
+   */
 	bool computerVision::isLeft() {
 		return (horizontalPixels < H_CENTERLEFT && horizontalPixels != -1);
 	}
 
+  /*
+   * Returns true if the shoelaces are too far to the right,
+   * false otherwise.
+   */
 	bool computerVision::isRight() {
 		return (horizontalPixels > H_CENTERRIGHT && horizontalPixels != -1);
 	}
 
+  /*
+   * Returns true if the shoelaces are too low in the frame,
+   * false otherwise.
+   */
 	bool computerVision::isTooLow() {
 		return (verticalPixels > V_CENTERBOTTOM && verticalPixels != -1);
 	}
 
+  /*
+   * Returns true if the shoeslaces are too high up in the
+   * frame, false otherwise.
+   */
 	bool computerVision::isTooHigh() {
 		return (verticalPixels < V_CENTERTOP && verticalPixels != -1);
 	}
 
+  /*
+   * Returns true if the shoes are within the specified
+   * range of the proximity sensor, false otherwise.
+   */
 	bool computerVision::inRange() {
 		return (this->proxDistance <= INRANGE); 
 	}
 
-	// get pixelFactor experimentally
+  /*
+   * Given the numeric code for which measurement to take,
+   * calculates the pixel difference and returns it.
+   */
 	double computerVision::getDistanceDifference(int code) {
 		double difference;
 		if (code == 0) {
@@ -88,14 +119,26 @@ namespace cv {
 		return difference * pixelFactor;
 	}
 
+  /*
+   * Returns true if the CV no longer sees the shoelaces,
+   * false otherwise.
+   */
 	bool computerVision::hasCut() {
 		return (this->verticalPixels == -1 && this->horizontalPixels == -1);
 	}
 
+  /*
+   * Returns the current proximity sensor reading.
+   */
 	int computerVision::getProxDistance() {
 		return this->proxDistance;
 	}
 
+  /*
+   * Given a string and index to begin reading from,
+   * parses the string until it reaches the deliminating
+   * character.
+   */
 	int computerVision::readString(const char *pipeString, int index) {
     std::string s;
     std::string str("_");
@@ -107,6 +150,11 @@ namespace cv {
 		return std::stoi(s);		
 	}
 
+  /*
+   * Given the name of a fifo pipe,
+   * reads input into a string, parses the information,
+   * and saves it.
+   */
 	void computerVision::readPipe(const char *pipe) {
 
 		std::ifstream ifs;
@@ -115,10 +163,8 @@ namespace cv {
 		char pipeString[14]; 
 		ifs.get(pipeString, 14);
 		
-
 		// Sets the fields by reading the pipe
 		this->horizontalPixels = readString(pipeString, 0);
-
 		this->verticalPixels = readString(pipeString, 4);
 
 		int tempDistance = readString(pipeString, 8);
@@ -191,11 +237,16 @@ namespace cv {
    * ======================================================
    **/
 
-  /* General constructor for previous RC set to FC */
+  /*
+   * General constructor for previous RC set to FC.
+   */
 	prevRC::prevRC() {
 		prevRC(0,0,0,0,0,0,0,0);	
 	}
 
+  /*
+   * Constructs prevRC with the given drone RC values.
+   */
   prevRC::prevRC(int roll, int pitch, int yaw, int throttle, int aux1, int aux2, int aux3, int aux4) {
     this->values[0] = roll;
     this->values[1] = pitch;
@@ -207,38 +258,66 @@ namespace cv {
     this->values[7] = aux4;
   }
 
+  /*
+   * Returns the current ROLL value.
+   */
   int prevRC::getRoll() {
   	return this->values[0];
   }
 
+  /*
+   * Returns the current PITCH value.
+   */
   int prevRC::getPitch() {
   	return this->values[1];
   }
 
+  /*
+   * Returns the current YAW value.
+   */
   int prevRC::getYaw() {
   	return this->values[2];
   }
 
+  /*
+   * Returns the current THROTTLE value.
+   */
   int prevRC::getThrottle() {
   	return this->values[3];
   }
 
+  /*
+   * Returns the current AUX1 value.
+   */
   int prevRC::getAux1() {
   	return this->values[4];
   }
 
+  /*
+   * Returns the current AUX2 value.
+   */
   int prevRC::getAux2() {
   	return this->values[5];
   }
 
+  /*
+   * Returns the current AUX3 value.
+   */
   int prevRC::getAux3() {
   	return this->values[6];
   }
 
+  /*
+   * Returns the current AUX4 value.
+   */
   int prevRC::getAux4() {
   	return this->values[7];
   }
 
+  /*
+   * Given the array of channels to change and their values,
+   * resets the values of the RC channels.
+   */
   void prevRC::update(int channels[], int values[]) {
   	int valuesIndex = 0;
   	for (int i = 0; i < 8; i++) {
@@ -246,8 +325,6 @@ namespace cv {
   			this->values[i] = values[i];
   			valuesIndex++;
   		}
-
   	}
-
   }
 }
